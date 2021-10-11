007c865e4da86aeeedbeb295a0e4d93f46baf790
Tt6588 <tatsusato1011@gmail.com>
2021-10-11 01:52:55 +0000

    [start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db32a7d811609711ef8d40fb7a8fd8c73f671427c5a043e3f76f85afadd520874a8011c6ca8cfeab871b9da464f6be99e6add779ea4616ef6a8dbae031a828e7'
